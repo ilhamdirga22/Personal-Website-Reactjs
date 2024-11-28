e91a08b09dce7e6f82e29251a22ac38215e21a1b
M Ilham Dirgantara <120619156+ilhamdirga22@users.noreply.github.com>
2024-11-28 09:51:30 +0800

    Update HomeSection.jsx

--- src/data/HomeSection.jsx
+++ src/data/HomeSection.jsx
@@ -1,5 +1,5 @@
 export const homeSection = {
-    image : '../img/ilham 2.png',
+    image : 'https://cdn-thumbs.imagevenue.com/70/9c/06/ME19ITW7_t.png',
     content :`<p className="deskripsi">Learn Programming With IlhamDirga #atHome</p>
                 <h2>Web Developer | Remote Worker</h2>
                 <p>Building exceptional web experiences from anywhere, one line of code at a time.</p>
